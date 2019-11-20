9afc07dad291e4b8ba6bda533870f3955537291d
Grzegorz Zaręba <g.zareba@selleo.com>
2019-11-20 18:17:19 +0100

    Generator

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import React from "react";
+import CanvasJSReact from "./lib/canvasjs.react";
+import { interval } from "rxjs";
+import { map, scan, share } from "rxjs/operators";
 
-import { interval, range } from "rxjs";
-import {
-  map,
-  take,
-  toArray,
-  bufferCount,
-  startWith,
-  bufferTime,
-  takeLast,
-  scan
-} from "rxjs/operators";
+const CanvasJSChart = CanvasJSReact.CanvasJSChart;
 
-const values = ({
+const generator = ({
   bitcoin: { min: btcMin, max: btcMax },
   etherum: { min: ethMin, max: ethMax }
 }) => {
-  const btc = interval(1000)
-    .pipe(map(() => Math.random()))
+  const timer = interval(1000).pipe(share());
+
+  const btc = streamBuilder(timer, btcMin, btcMax);
+  const eth = streamBuilder(timer, ethMin, ethMax);
+
+  return { btc, eth };
+};
+
+const streamBuilder = (timer, min, max) =>
+  timer
+    .pipe(map(() => getRandomIntInclusive(min, max)))
+    .pipe(map(value => ({ x: new Date(), y: value })))
     .pipe(
       scan((acc, val) => {
         acc.push(val);
@@ -25,23 +28,68 @@ const values = ({
       }, [])
     );
 
-  return btc;
+const getRandomIntInclusive = (min, max) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
+const chartFrame = (btcValues, ethValues) => ({
+  theme: "light2",
+  title: {
+    text: "BTC & ETH"
+  },
+  axisY: {
+    includeZero: false,
+    prefix: "$"
+  },
+  toolTip: {
+    shared: true
+  },
+  data: [
+    {
+      type: "area",
+      name: "BTC",
+      showInLegend: true,
+      yValueFormatString: "$#,##0.##",
+      dataPoints: btcValues
+    },
+    {
+      type: "area",
+      name: "ETH",
+      showInLegend: true,
+      yValueFormatString: "$#,##0.##",
+      dataPoints: ethValues
+    }
+  ]
+});
+
 const App = () => {
-  const [stream, setStream] = React.useState();
+  const [btcStream, setBtcStream] = React.useState();
+  const [ethStream, setEthStream] = React.useState();
 
   React.useEffect(() => {
-    values({
-      bitcoin: { min: 1, max: 5 },
-      etherum: { min: 20, max: 30 }
-    }).subscribe(val => {
-      console.log(val);
-      setStream(val);
+    const values = generator({
+      bitcoin: { min: 10000, max: 10500 },
+      etherum: { min: 3000, max: 3200 }
+    });
+
+    values.btc.subscribe(val => {
+      console.log("BTC:", val);
+      setBtcStream(val);
+    });
+
+    values.eth.subscribe(val => {
+      console.log("ETH:", val);
+      setEthStream(val);
     });
   }, []);
 
-  return <div className="App">{stream}</div>;
+  return (
+    <div className="App">
+      <CanvasJSChart options={chartFrame(btcStream, ethStream)} />
+    </div>
+  );
 };
 
 export default App;
